9113803a5fbba0a63f771188b262765519cbbc1e
Quincy Liang <mblquincy@outlook.com>
2018-01-27 20:39:37 +0800

    Create package structure of the project
    Since it is my repository for learning algorithms and is hosted on
    GitHub, I just name the package 'com.github.deltaquincy.algorithms'.
    This commit creates the general structure, and adds a main class
    with an initial test script.

---
+++ src/main/java/com/github/deltaquincy/algorithms/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2018 Quincy Liang
+ *
+ * Project Algorithms. A repository of my algorithms learning.
+ * Licensed under the MIT license.
+ */
+
+package com.github.deltaquincy.algorithms;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Main entry of project Algorithms.
+ *
+ * @author Quincy Liang
+ */
+public class Main {
+  public static boolean readyForTesting() {
+    return true;
+  }
+
+  public static void main(String[] args) {
+    StdOut.println("Hello, world!");
+  }
+}
---
+++ src/test/java/com/github/deltaquincy/algorithms/MainTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2018 Quincy Liang
+ *
+ * Project Algorithms. A repository of my algorithms learning.
+ * Licensed under the MIT license.
+ */
+
+package com.github.deltaquincy.algorithms;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.github.deltaquincy.algorithms.Main;
+
+public class MainTest {
+  @Test
+  public void testMainIsReadyForTesting() {
+    assertTrue("Main class should be ready for testing.",
+    Main.readyForTesting());
+  }
+}
