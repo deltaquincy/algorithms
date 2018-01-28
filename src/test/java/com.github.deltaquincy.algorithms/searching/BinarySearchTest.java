1694119aa48aeddf8d3855b01846065e6b31632b
Quincy Liang <mblquincy@outlook.com>
2018-01-28 11:45:22 +0800

    Add a test case for binary search algorithm
    According to the book algs4, this commit add two test data files and
    a new test case for binary search algorithm.

--- src/test/java/com.github.deltaquincy.algorithms/searching/BinarySearchTest.java
+++ src/test/java/com.github.deltaquincy.algorithms/searching/BinarySearchTest.java
@@ -11,6 +11,8 @@ import java.util.Arrays;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import edu.princeton.cs.algs4.In;
 
 import com.github.deltaquincy.algorithms.searching.BinarySearch;
 
@@ -27,5 +29,22 @@ public class BinarySearchTest {
     int[] testArray = {58, 72, 3, 90, 15, 8, 23, 18, 42};
     Arrays.sort(testArray);
     assertEquals(BinarySearch.rank(70, testArray), -1);
+  }
+
+  @Test
+  public void testWhiteList() {
+    int[] whitelist = In.readInts("testdata/tinyW.txt");
+    Arrays.sort(whitelist);
+    In target = new In("testdata/tinyT.txt");
+    int[] goldresult = {50, 99, 13};
+    int[] result = new int[goldresult.length];
+    for (int i = 0; !target.isEmpty(); ) {
+      int key = target.readInt();
+      if (BinarySearch.rank(key, whitelist) < 0) {
+        result[i] = key;
+        i++;
+      }
+    }
+    assertArrayEquals(goldresult, result);
   }
 }
