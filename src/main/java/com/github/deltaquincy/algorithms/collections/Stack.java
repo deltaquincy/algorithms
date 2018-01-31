1252f1e2c5f9566b4af3fb65e4ef9840ba5b3ff2
Quincy Liang <mblquincy@outlook.com>
2018-02-01 06:26:30 +0800

    Add tests and javadoc to 'Stack' implementation

--- src/main/java/com/github/deltaquincy/algorithms/collections/Stack.java
+++ src/main/java/com/github/deltaquincy/algorithms/collections/Stack.java
@@ -7,6 +7,13 @@ package com.github.deltaquincy.algorithms.collections;
 
 import java.util.Iterator;
 
+/**
+ * The <code>Stack</code> class is a <i>LIFO</i> data structure. In this
+ * version, the implementation uses linked list to realize variable length
+ * mechanism.
+ *
+ * @author Quincy Liang
+ */
 public class Stack<Item> implements Iterable<Item> {
     private Node first;  // Top of the stack.
     private int N;       // Length of the stack.
@@ -17,14 +24,30 @@ public class Stack<Item> implements Iterable<Item> {
         Node next;
     }
 
+    /**
+     * Check if the stack is empty.
+     *
+     * @return true if the stack is empty, in other words, its length is 0
+     */
     public boolean isEmpty() {
         return first == null;
     }
 
+    /**
+     * Get the current size of the stack.
+     *
+     * @return the current size, or length, of the stack
+     */
     public int size() {
         return N;
     }
 
+    /**
+     * Push an item into top of the stack. This would add <code>1</code> to the
+     * stack length.
+     *
+     * @param item the item to push into the stack
+     */
     public void push(Item item) {
         Node oldfirst = first;
         first = new Node();
@@ -33,6 +56,12 @@ public class Stack<Item> implements Iterable<Item> {
         N++;
     }
 
+    /**
+     * Pop an item from top of the stack. This would delete the item from the
+     * stack and minus <code>1</code> to the stack length.
+     *
+     * @return the item just poped from the stack
+     */
     public Item pop() {
         Item item = first.item;
         first = first.next;
@@ -40,6 +69,16 @@ public class Stack<Item> implements Iterable<Item> {
         return item;
     }
 
+    /**
+     * Implement the iterable interface. For <code>Stack</code>, the iteration
+     * would be from the top to the bottom of the stack, corresponding to the
+     * <i>LIFO</i> logic.
+     *
+     * @return an appropriate iterator
+     *
+     * @see java.lang.Iterable
+     * @see java.util.Iterator
+     */
     public Iterator<Item> iterator() {
         return new LinkedListStackIterator();
     }
---
+++ src/test/java/com/github/deltaquincy/algorithms/collections/StackTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 Quincy Liang
+ * Under the CPL v3 license
+ */
+
+package com.github.deltaquincy.algorithms.collections;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import com.github.deltaquincy.algorithms.collections.Stack;
+
+public class StackTest {
+    private Stack<Integer> stack = new Stack<Integer>();
+
+    @Test
+    public void testInitializeStack() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testPushAndPopItems() {
+        int itemToPush = 1996;
+        stack.push(itemToPush);
+        int itemPoped = stack.pop();
+        assertEquals(itemToPush, itemPoped);
+    }
+
+    @Test
+    public void testIteration() {
+        int[] itemsToPush = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int itemToPush : itemsToPush) {
+            stack.push(itemToPush);
+        }
+        assertEquals(stack.size(), 10);
+        for (int itemToPop : stack) {
+            int itemPoped  = stack.pop();
+            assertEquals(itemToPop, itemPoped);
+        }
+        assertTrue(stack.isEmpty());
+    }
+}
