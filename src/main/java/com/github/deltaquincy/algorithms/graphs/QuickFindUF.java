5934497cbde331634d7b9f0c144b94a7dfa19631
Quincy Liang <mblquincy@outlook.com>
2018-02-04 13:06:29 +0800

    Add union-find sample project
    In this commit, union-find algorithm in book section 1.5 is implemented.

---
+++ src/main/java/com/github/deltaquincy/algorithms/graphs/QuickFindUF.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2018 Quincy Liang
+ * Under the GPL v3 license
+ */
+
+package com.github.deltaquincy.algorithms.graphs;
+
+public class QuickFindUF extends AbstractUF {
+    public QuickFindUF(int N) {
+        super(N);
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public void union(int p, int q) {
+        int pId = find(p);
+        int qId = find(q);
+        if (pId == qId) return;
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pId) id[i] = qId;
+        }
+        count--;
+    }
+}
---
+++ src/main/java/com/github/deltaquincy/algorithms/graphs/WeightedQuickUnionUF.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2018 Quincy Liang
+ * Under the GPL v3 license
+ */
+
+package com.github.deltaquincy.algorithms.graphs;
+
+public class WeightedQuickUnionUF extends AbstractUF {
+    private int[] sz;
+
+    public WeightedQuickUnionUF(int N) {
+        super(N);
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            sz[i] = i;
+        }
+    }
+
+    public int find(int p) {
+        while (p != id[p]) p = id[p];
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+        if (sz[i] < sz[j]) {
+            id[i] = id[j];
+            sz[j] += sz[i];
+        } else {
+            id[j] = id[i];
+            sz[i] += sz[j];
+        }
+        count--;
+    }
+}
